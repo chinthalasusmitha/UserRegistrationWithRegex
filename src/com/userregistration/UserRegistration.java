49815153032eb0d786a452f0b1841ccbc7b86202
susmitha <susmithareddychinthala024@gmail.com>
2022-11-15 18:34:38 +0530

    Added: Code to check Weather the first Name is valid or not

---
+++ src/com/userregistration/UserRegistration.java
@@ -0,0 +1,38 @@
+package com.userregistration;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    static ArrayList<String> list = new ArrayList<>();
+
+    private static void validFirstName() {
+        System.out.println("Welcome To User Registration");
+        String firstName = "Pinky";
+        boolean result = validName(firstName);
+        System.out.println("First name: " + result);
+    }
+
+    private static boolean validName(String name) {
+        boolean result = false;
+        String regex = "[A-Z]{1}[a-z]{3,}";// {3,} means above 3
+        Pattern pattern = Pattern.compile(regex);
+        for (int i = 0; i < name.length(); i++) {
+            Matcher matcher = pattern.matcher(name);
+            result = matcher.matches();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        UserRegistration user = new UserRegistration();
+        user.validFirstName();
+    }
+
+
+}
+
+
+
